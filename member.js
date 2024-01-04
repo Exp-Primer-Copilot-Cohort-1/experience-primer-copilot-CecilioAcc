df73ad1d28470dfdb879f09d36e127c83323b89f
Cecilio <31824554+CecilioAcc@users.noreply.github.com>
2024-01-04 09:42:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: skills,
+    };
+    console.log(member);
+}
